e6b20d83f3d0c10c439840dadffaf41aeccce9cf
kawsar1331 <60744000+kawsar1331@users.noreply.github.com>
2022-09-20 03:59:55 +0600

    vid8e

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,28 +1,37 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import data from '../data';
+import axios from 'axios';
+// import data from '../data';
 
 function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data);
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>Featured Products</h1>
-      <div className="products">
-        {data.products.map((product) => (
-          <div className="product" key={product.slug}>
+      <div className="products"></div>
+      {products.map((product) => (
+        <div className="product" key={product.slug}>
+          <Link to={`/product/${product.slug}`}>
+            <img src={product.image} alt={product.name} />
+          </Link>
+          <div className="product-info">
             <Link to={`/product/${product.slug}`}>
-              <img src={product.image} alt={product.name} />
+              <p>{product.name}</p>
             </Link>
-            <div className="product-info">
-              <Link to={`/product/${product.slug}`}>
-                <p>{product.name}</p>
-              </Link>
-              <p>
-                <strong>${product.price}</strong>
-              </p>
-              <button>Add to cart</button>
-            </div>
+            <p>
+              <strong>${product.price}</strong>
+            </p>
+            <button>Add to cart</button>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
   );
 }
